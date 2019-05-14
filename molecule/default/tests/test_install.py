c3b581850221a503f5d190a6f71801dc5c7ad216
killerwhile <benoit@noisette.ch>
2019-05-14 09:29:30 +0200

    Update molecule version in DevOps container.
    GitOrigin-RevId: a74c9892cec0e4e63515724ddbd0cd6069cc2d71

---
+++ molecule/default/tests/test_install.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name, version", [
+    ("docker-ce", "18.06"),
+])
+def test_yum_pkg_are_installed(host, name, version):
+    pkg = host.package(name)
+    assert pkg.is_installed
+    assert pkg.version.startswith(version)
